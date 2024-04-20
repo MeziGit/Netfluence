18f1aa56bc51ec85d50966284c8c138d2039b5fd
Ryanmeziane <2059188@johnabbottcollege.net>
2024-04-20 14:37:02 -0400

    netlify changes on development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    // Other configurations...
+  integrations: [react()]
+  // Other configurations...
+  ,
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
