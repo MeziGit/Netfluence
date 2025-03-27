74f8fbad3659df6d7176f2c0f2c5d07df9833ff3
ryanmeziane <2059188@johnabbottcollege.net>
2025-03-27 13:33:45 -0400

    edited clarity insight

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import Clarity from '@microsoft/clarity';
-
-// Initialize Microsoft Clarity
-Clarity.init("quub6qff3z");
 
 // Add global styles for tailwind components
 import './styles/globals.css';
